276270a0d77c3feafb085b506cba774d7f858dde
straightbuggin <alex.trei@gmail.com>
2023-10-04 15:00:18 -0700

    starting point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+use directories::UserDirs;
+
+
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Args {
+    #[clap(short = 'p', long ,env)]
+    garden_path: Option<PathBuf>,
+
+    #[command(subcommand)]
+    cmd: Commands,
+}
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// write something in your garden
+    ///
+    /// This command will open your $EDITOR, wait for you
+    /// to write something, and then save the file to your garden
+    Write {
+        #[clap(short, long)]
+        title: Option<String>,
+    },
+}
+
+fn get_default_garden_dir() -> Option<PathBuf> {
+UserDirs::new()
+    .map(|dirs| dirs.home_dir().join("garden"))
+}
+
 fn main() {
-todo!();
+    let args = Args::parse();
+    dbg!(args);
 }
